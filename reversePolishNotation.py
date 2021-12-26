62c8ab2686c945ea77b1193b7d87e9623ccf68a9
RF_BURGER <Arefyazdi4@gmail.com>
2021-12-26 20:04:27 +0330

    modify variable names

--- reversePolishNotation.py
+++ reversePolishNotation.py
@@ -3,26 +3,26 @@ from stackClass import Stack
 
 class Rpn:
     @staticmethod
-    def operator_applier(operator, *args):
+    def __operator_applier(operator, *args):
         if operator == '+':
             result = 0
             for num in args:
-                result = result + num
+                result += num
             return result
         elif operator == '*':
             result = 1
             for num in args:
-                result = result * num
+                result *= num
             return result
         elif operator == '-':
             result = args[0]
             for num in args[1:]:
-                result = result - num
+                result -= num
             return result
         elif operator == '/':
             result = args[0]
             for num in args[1:]:
-                result = result / num
+                result /= num
             return result
 
     @staticmethod
@@ -31,15 +31,15 @@ class Rpn:
         if root_node in ('+', '*', '-', '/'):
             right_node = Rpn.reverse_p_notation(arithmetic_expression)
             left_node = Rpn.reverse_p_notation(arithmetic_expression)
-            return Rpn.operator_applier(root_node, left_node, right_node)
+            return Rpn.__operator_applier(root_node, left_node, right_node)
         else:
-            return int(root_node)
+            return float(root_node)
 
 
 if __name__ == '__main__':
-    statements_list = '5 4 + 5 3 - *'.split()
+    statements_list = '70 2 4 1 - * 4 + / 3.5 0 + /'
     statements_stack = Stack()
-    for statement in statements_list:
+    for statement in statements_list.split():
         statements_stack.push(statement)
 
     print(Rpn.reverse_p_notation(statements_stack))
