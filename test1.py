5ea1ccb05337def9c64e48207cda4e9a606ab19c
RF_BURGER <Arefyazdi4@gmail.com>
2022-01-29 20:48:53 +0330

    graph_plot_matrix_libs

--- test1.py
+++ test1.py
@@ -91,91 +91,76 @@
 # if __name__ == '__main__':
 #     v = [float('inf')] * 5
 #     print(v)
+
+
+
+
+
 import networkx as nx
 import matplotlib.pyplot as plt
 
-# if __name__ == '__main__':
-    # G=nx.DiGraph()
-    # G.add_node(0),G.add_node(1),G.add_node(2),G.add_node(3),G.add_node(4)
-    # G.add_edge(0, 1),G.add_edge(1, 2),G.add_edge(0, 2),G.add_edge(1, 4),G.add_edge(1, 3),G.add_edge(3, 2),G.add_edge(3,1),G.add_edge(4,3)
-    # nx.draw(G, with_labels=True, font_weight='bold')
-    # plt.show()
-
-
-
-    # G = nx.cubical_graph()
-    # pos = nx.spring_layout(G, seed=3113794652)  # positions for all nodes
-    #
-    # # nodes
-    # options = {"edgecolors": "tab:gray", "node_size": 800, "alpha": 0.9}
-    # nx.draw_networkx_nodes(G, pos, nodelist=[0, 1, 2, 3], node_color="tab:red", **options)
-    # nx.draw_networkx_nodes(G, pos, nodelist=[4, 5, 6, 7], node_color="tab:blue", **options)
-    #
-    # # edges
-    # nx.draw_networkx_edges(G, pos, width=1.0, alpha=0.5)
-    # nx.draw_networkx_edges(
-    #     G,
-    #     pos,
-    #     edgelist=[(0, 1), (1, 2), (2, 3), (3, 0)],
-    #     width=8,
-    #     alpha=0.5,
-    #     edge_color="tab:red",
-    # )
-    # nx.draw_networkx_edges(
-    #     G,
-    #     pos,
-    #     edgelist=[(4, 5), (5, 6), (6, 7), (7, 4)],
-    #     width=8,
-    #     alpha=0.5,
-    #     edge_color="tab:blue",
-    # )
-    #
-    # # some math labels
-    # labels = {}
-    # labels[0] = r"$a$"
-    # labels[1] = r"$b$"
-    # labels[2] = r"$c$"
-    # labels[3] = r"$d$"
-    # labels[4] = r"$\alpha$"
-    # labels[5] = r"$\beta$"
-    # labels[6] = r"$\gamma$"
-    # labels[7] = r"$\delta$"
-    # nx.draw_networkx_labels(G, pos, labels, font_size=22, font_color="whitesmoke")
-    #
-    # plt.tight_layout()
-    # plt.axis("off")
-    # plt.show()
-
-
-
-# import numpy as np
-# import matplotlib.pylab as pl
-#
-# if __name__ == '__main__':
-#
-#     nx = 4
-#     ny = 5
-#     data = np.random.randint(0,10,size=(ny,nx))
-#
-#     pl.figure()
-#     tb = pl.table(cellText=data, loc=(0,0), cellLoc='center')
-#
-#     tc = tb.properties()['child_artists']
-#     for cell in tc:
-#         cell.set_height(1/ny)
-#         cell.set_width(1/nx)
-#
-#     ax = pl.gca()
-#     ax.set_xticks([])
-#     ax.set_yticks([])
-import matplotlib.pyplot as plt
-import numpy as np
-
 if __name__ == '__main__':
-
-    a = [[0 for i in range(100)] for _ in range(100)]
-    a[2][2]=1
-    # a = np.random.randint(-1, 2, (5, 5))
-    plt.ion()
-    plt.imshow(a, interpolation='none')
+    G1=nx.DiGraph()
+    G1.add_node(0),G1.add_node(1),G1.add_node(2),G1.add_node(3),G1.add_node(4)
+    G1.add_edge(0, 1),G1.add_edge(1, 2),G1.add_edge(0, 2),G1.add_edge(1, 4),G1.add_edge(1, 3),G1.add_edge(3, 2),G1.add_edge(3,1),G1.add_edge(4,3)
+    nx.draw(G1, with_labels=True, font_weight='bold')
     plt.show()
+
+
+
+    G = nx.cubical_graph()
+    pos = nx.spring_layout(G, seed=3113794652)  # positions for all nodes
+
+    # nodes
+    options = {"edgecolors": "tab:gray", "node_size": 800, "alpha": 0.9}
+    nx.draw_networkx_nodes(G, pos, nodelist=[0, 1, 2, 3], node_color="tab:red", **options)
+    nx.draw_networkx_nodes(G, pos, nodelist=[4, 5, 6, 7], node_color="tab:blue", **options)
+
+    # edges
+    nx.draw_networkx_edges(G, pos, width=1.0, alpha=0.5)
+    nx.draw_networkx_edges(
+        G,
+        pos,
+        edgelist=[(0, 1), (1, 2), (2, 3), (3, 0)],
+        width=8,
+        alpha=0.5,
+        edge_color="tab:red",
+    )
+    nx.draw_networkx_edges(
+        G,
+        pos,
+        edgelist=[(4, 5), (5, 6), (6, 7), (7, 4)],
+        width=8,
+        alpha=0.5,
+        edge_color="tab:blue",
+    )
+
+    # some math labels
+    labels = {}
+    labels[0] = r"$a$"
+    labels[1] = r"$b$"
+    labels[2] = r"$c$"
+    labels[3] = r"$d$"
+    labels[4] = r"$\alpha$"
+    labels[5] = r"$\beta$"
+    labels[6] = r"$\gamma$"
+    labels[7] = r"$\delta$"
+    nx.draw_networkx_labels(G, pos, labels, font_size=22, font_color="whitesmoke")
+
+    plt.tight_layout()
+    plt.axis("off")
+    plt.show()
+
+
+
+# import matplotlib.pyplot as plt
+# import numpy as np
+#
+# if __name__ == '__main__':
+#
+#     a = [[0 for i in range(100)] for _ in range(100)]
+#     a[2][2]=1
+#     # a = np.random.randint(-1, 2, (5, 5))
+#     plt.ion()
+#     plt.imshow(a, interpolation='none')
+#     plt.show()
